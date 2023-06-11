64a1b34e345ecb9d0f63a1c6dfa54efeed068207
JaekSooley <117260365+JaekSooley@users.noreply.github.com>
2023-06-11 20:45:07 +1000

    Initial commit

---
+++ ConsoleUI.cs
@@ -0,0 +1,138 @@
+﻿namespace ConsoleUI
+{
+    public class UI
+    {
+        public static void Header(string header, bool clearPreviousScreen = true)
+        {
+            Console.WriteLine("");
+            if (clearPreviousScreen) Console.Clear();
+            else Console.WriteLine("------------------------------------------");
+            Console.WriteLine(header.ToUpper());
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("");
+        }
+
+        public static void Option(string option, string description = "", string current = "")
+        {
+            string text = $"\t{option}";
+            if (current != "") text += $" ({current})";
+            if (description != "") text += $"\t\t- {description}";
+            Console.WriteLine(text);
+        }
+
+        public static void Pause()
+        {
+            Console.WriteLine("");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void Error(string description)
+        {
+            Header("///// Error /////");
+            Console.WriteLine(description);
+            Pause();
+        }
+
+        public static void Warning(string description)
+        {
+            Header("Warning", false);
+            Console.WriteLine(description);
+        }
+
+        public static void Input(string type, string defaultValue = "")
+        {
+            Console.WriteLine("");
+            if (defaultValue == "") Console.Write($"Input ({type}): ");
+            else Console.Write($"Input ({type}): [{defaultValue}] ");
+        }
+    }
+
+    public class Input
+    {
+        public static bool GetBoolean(bool defaultValue = false)
+        {
+            string type = "bool";
+            UI.Input("bool", defaultValue.ToString());
+            string input = ReadLine();
+            bool output = defaultValue;
+            if (bool.TryParse(input, out bool val)) output = val;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+        public static int GetInteger(int defaultValue = 0)
+        {
+            string type = "int";
+            UI.Input("int", defaultValue.ToString());
+            string input = ReadLine();
+            int output = defaultValue;
+            if (int.TryParse(input, out int val)) output = val;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+
+        public static float GetFloat(float defaultValue = 0f)
+        {
+            string type = "float";
+            UI.Input(type, defaultValue.ToString());
+            string input = ReadLine();
+            float output = defaultValue;
+            if (float.TryParse(input, out float val)) output = val;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+
+        public static double GetDouble(double defaultValue = 0)
+        {
+            string type = "double";
+            UI.Input(type, defaultValue.ToString());
+            string input = ReadLine();
+            double output = defaultValue;
+            if (double.TryParse(input, out double val)) output = val;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+
+
+        public static decimal GetDecimal(decimal defaultValue = 0m)
+        {
+            string type = "decimal";
+            UI.Input(type, defaultValue.ToString());
+            string input = ReadLine();
+            decimal output = defaultValue;
+            if (decimal.TryParse(input, out decimal val)) output = val;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+
+        public static string GetString(string defaultValue = "")
+        {
+            string type = "string";
+            UI.Input(type, defaultValue.ToString());
+            string input = ReadLine();
+            string output = defaultValue;
+            if (input != "") output = input;
+            else InputInvalid(type, input, defaultValue.ToString());
+
+            return output;
+        }
+
+        static string ReadLine()
+        {
+            string? input = Console.ReadLine();
+            if (input == null) input = "";
+
+            return input;
+        }
+
+        static void InputInvalid(string type, string input, string valueSetTo)
+        {
+            UI.Warning($"\"{input}\" was not of type \"{type}\". Default value \"{valueSetTo}\" was used.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleUI;
+using System;
+using System.Xml;
+
+
+List<string> elementList = new List<string>(); // List of elements to remove
+
+bool mainLoop = true;
+
+elementList.Add("PlanFeatures");
+elementList.Add("SourceData");
+
+while (mainLoop)
+{
+    UI.Header("Home");
+    Console.WriteLine("This program removes unwanted elements from LandXML files to reduce size and remove breaklines when importing into drawings.");
+    Console.WriteLine("");
+    Console.WriteLine("Drag and drop xml file and press ENTER to begin.");
+    Console.WriteLine("");
+    UI.Option("[S]ETTINGS");
+    UI.Option("E[X]IT");
+
+    string input = Input.GetString();
+
+    input = input.Replace("\"", "");
+
+    switch (input.ToUpper())
+    {
+
+        // More options here.
+        case "S":
+        case "SETTINGS":
+            UI.Header("Settings");
+            Settings();
+            break;
+
+        case "X":
+        case "EXIT":
+            mainLoop = false;
+            break;
+
+        case "":
+            break;
+
+        default:
+            // Open the file and do the thing
+            if (File.Exists(input))
+            {
+                ProcessXmlFile(input);
+            }
+            else
+            {
+                UI.Header("Error");
+                Console.WriteLine("Unrecognised command!");
+                UI.Pause();
+            }
+            break;
+    }
+}
+
+UI.Header("Goodbye");
+
+void Settings()
+{
+    UI.Header("Settings");
+    Console.WriteLine("Elements to Remove:");
+
+    foreach (string element in elementList)
+    {
+        Console.WriteLine($"\t{element}");
+    }
+
+    UI.Pause();
+}
+
+void ProcessXmlFile(string fname)
+{
+    UI.Header("Cleaning LandXML file...");
+
+    XmlDocument xmlDoc = new XmlDocument();
+    xmlDoc.Load(fname);
+
+    for (int i = 0; i < elementList.Count; i++)
+    {
+        Console.WriteLine($"\tRemoving \"{elementList[i]}\"... ");
+
+        XmlNodeList elementsToRemove = xmlDoc.GetElementsByTagName(elementList[i]);
+
+        if (elementsToRemove.Count > 0)
+        {
+            for (int j = elementsToRemove.Count - 1; j >= 0; j--)
+            {
+                XmlNode element = elementsToRemove[j];
+
+                if (element != null)
+                {
+                    if (element.ParentNode != null)
+                    {
+                        element.ParentNode.RemoveChild(element);
+                    }
+                    else
+                    {
+                        UI.Error("ParentNode returned null.");
+                    }
+                }
+                else
+                {
+                    UI.Error($"element returned null.");
+                }
+            }
+
+            xmlDoc.Save(fname);
+        }
+        else
+        {
+            UI.Error($"No matching element \"{elementList[i]}\" found in the XML file.");
+        }
+    }
+
+    Console.WriteLine("Done!");
+
+    UI.Pause();
+}
